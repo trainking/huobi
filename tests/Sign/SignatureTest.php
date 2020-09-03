dcfb12e7b985d1a4f54ceab0ea109ab79f3ae818
trainking <yefulincode@gmail.com>
2020-09-03 14:14:39 +0800

    add phpunit

---
+++ tests/Sign/SignatureTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Huobi\Tests\Sign;
+
+use PHPUnit\Framework\TestCase;
+
+class SignatureTest extends TestCase
+{
+    public function testSign()
+    {
+        echo "hello!";
+    }
+}
